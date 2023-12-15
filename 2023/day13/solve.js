ac9f148ab0da832d20f9487d4ad01a7663b141a5
kien <akitak1290@gmail.com>
2023-12-15 01:39:10 -0500

    working on solve for part 2

--- 2023/day13/solve.js
+++ 2023/day13/solve.js
@@ -1,29 +1,62 @@
 const { readFileSync } = require('fs')
 const { resolve } = require('path')
 
-function countRow(block) {
+// assume str1 and str2 have the same length
+// and are both string
+function compareString(str1, str2, len) {
+	let dif = 0
+	for (let i = 0; i < len; i++) {
+		if (str1[i] !== str2[i]) dif += 1
+	}
+	return dif
+}
+
+/**
+ * 
+ * @param {string[]} block 
+ * @param {number} smudgeCount
+ * @param {number | undefined} exclude
+ * @returns {number | null}
+ */
+function countRow(block, smudgeCount) {
 	let rowIdx = 0
 	while (rowIdx < block.length - 1) {
-		if (block[rowIdx] === block[rowIdx + 1]) {
+		let dif = compareString(block[rowIdx], block[rowIdx + 1])
+		// TODO: prob won't need this if and merge it with while
+		if (dif === 0 || dif === 1) {
 			let offset = 0
 			let fullMatch = true
 			// search outward
-			while (offset <= Math.min(rowIdx, block.length - rowIdx - 2)) {
-				fullMatch = fullMatch && block[rowIdx - offset] === block[rowIdx + offset + 1]
+			while (rowIdx - offset >= 0 && rowIdx + offset + 1 <= block.length - 1) {
+				dif = compareString(block[rowIdx - offset], block[rowIdx + offset + 1], block[0].length)
+				if (dif > 0) {
+					if (dif === 1 && smudgeCount > 0) {
+						smudgeCount -= 1
+					} else {
+						fullMatch = false
+						break
+					}
+				}
 				offset += 1
 			}
-			if (fullMatch) return rowIdx + 1
+			if (fullMatch && smudgeCount === 0) {
+				return rowIdx + 1
+			}
 		}
 		rowIdx += 1
 	}
-
-	return 0
+	return null
 }
 
-function countTerrain(block) {
-	// count row
-	const rows = countRow(block)*100
+/**
+ * 
+ * @param {string[]} block 
+ * @param {number} smudgeCount
+ * @returns {number[]}
+ */
+function countTerrain(block, smudgeCount) {
 	// count column
+	// TODO: refac this?
 	let tmp = []
 	for (let i = 0; i < block[0].length; i++) {
 		let line = ''
@@ -32,29 +65,37 @@ function countTerrain(block) {
 		}
 		tmp.push(line)
 	}
-	const cols = countRow(tmp)
+	const col = countRow(tmp, smudgeCount)
+	// count row
+	const row = countRow(block, smudgeCount)
 
-	return rows + cols
+	return row*100 + col
 }
 
 const input = readFileSync(resolve('./input.txt'), { encoding: 'utf-8' })
 const lines = input.split('\n')
 
+const blocks = []
 let block = []
-let result = 0
-for (const [idx, line] of lines.entries()) {
-	if (line === '') {
-		result += countTerrain(block)
-		block = []
-		continue
+let idx = 0
+while (idx < lines.length){
+	if (lines[idx] !== '') {
+		block.push(lines[idx])
+		if (idx === lines.length - 1 || lines[idx+1] === '') {
+			blocks.push(block)
+			block = []
+		}
 	}
-
-	if (idx === lines.length - 1) {
-		block.push(line)
-		result += countTerrain(block)
-	}
-
-	block.push(line)
+	idx += 1
 }
 
-console.log(result)
+let resultP1 = 0
+let resultP2 = 0
+for (const block of blocks) {
+	// the parsing thing to add 1 is an eyesore...
+	// TODO: update logic?
+	resultP1 += countTerrain(block, 0)
+	resultP2 += countTerrain(block, 1)
+}
+
+console.log(resultP1, resultP2)
