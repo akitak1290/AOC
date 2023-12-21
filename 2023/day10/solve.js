ba83a7dea3209b2336d9512d315b5faf7995099d
kien <akitak1290@gmail.com>
2023-12-21 01:28:20 -0500

    solve for p1 and p2

--- 2023/day10/solve.js
+++ 2023/day10/solve.js
@@ -1,63 +1,174 @@
 const { readFileSync } = require('node:fs')
 
-function getDirection(speed, char) {
-	let newSpeed
-	if (speed[0] == 1) {
-		// right
-		if (char == "-") newSpeed = [1, 0]
-		if (char == "J") newSpeed = [0, -1]
-		if (char == "7") newSpeed = [0, 1]
-	}
-	else if (speed[0] == -1) {
-		// left
-		if (char == "-") newSpeed = [-1, 0]
-		if (char == "L") newSpeed = [0, -1]
-		if (char == "F") newSpeed = [0, 1]
-	}
-	else if (speed[1] == 1) {
-		// bottom
-		if (char == "|") newSpeed = [0, 1]
-		if (char == "L") newSpeed = [1, 0]
-		if (char == "J") newSpeed = [-1, 0]
-	} else if (speed[1] == -1) {
-		// top
-		if (char == "|") newSpeed = [0, -1]
-		if (char == "F") newSpeed = [1, 0]
-		if (char == "7") newSpeed = [-1, 0]
-	}
+// 1. scale the graph x3 so fill can work with paths
+// from outside to in with not actual gap, but there are paths
+// 2. follow the loop to count its length, asw for p1 is length/2
+// 3. fill the whole graph, after that, count the 'actual cell'
+// (which are not 'scaled cells') that has not been filled
+// asw for p2
 
-	if (!newSpeed) throw new Error('char is not valid');
-
-	return newSpeed
+function scaleUp(lines) {
+	const newLines = []
+	for (const line of lines) {
+		const scaledLine = [[], [], []]
+		for (const char of line) {
+			const scaledChar = Array.from(new Array(3), () => new Array(3).fill('.'))
+			if (char === '|') {
+				scaledChar[0][1] = '|'
+				scaledChar[1][1] = '|'
+				scaledChar[2][1] = '|'
+			} else if (char === '-') {
+				scaledChar[1][0] = '-'
+				scaledChar[1][1] = '-'
+				scaledChar[1][2] = '-'
+			} else if (char === 'L') {
+				scaledChar[0][1] = '|'
+				scaledChar[1][1] = 'L'
+				scaledChar[1][2] = '-'
+			} else if (char === 'J') {
+				scaledChar[0][1] = '|'
+				scaledChar[1][1] = 'J'
+				scaledChar[1][0] = '-'
+			} else if (char === '7') {
+				scaledChar[1][0] = '-'
+				scaledChar[1][1] = '7'
+				scaledChar[2][1] = '|'
+			} else if (char === 'F') {
+				scaledChar[1][2] = '-'
+				scaledChar[1][1] = 'F'
+				scaledChar[2][1] = '|'
+			}
+			scaledLine[0].push(...scaledChar[0])
+			scaledLine[1].push(...scaledChar[1])
+			scaledLine[2].push(...scaledChar[2])
+		}
+		for (const sLine of scaledLine) {
+			newLines.push(sLine)
+		}
+	}
+	return newLines.map((line) => line.join(''))
 }
 
-// prob can refactor this
-function solve(lines, start, pos1, pos2, speed1, speed2) {
-	let visited = new Set()
+function fill(x, y, visited) {
+	const queue = []
 
-	visited.add(JSON.stringify(start))
-	visited.add(JSON.stringify(pos1))
-	visited.add(JSON.stringify(pos2))
+	queue.push([x, y])
+	visited[y][x] = true
+	while (queue.length > 0) {
+		const cur = queue.shift()
+		x = cur[0]
+		y = cur[1]
 
-	while (JSON.stringify(pos1) != JSON.stringify(pos2)) {
-		// update pos
-		pos1 = pos1.map((v, i) => v+speed1[i])
-		pos2 = pos2.map((v, i) => v+speed2[i])
-
-		// update set
-		visited.add(JSON.stringify(pos1))
-		visited.add(JSON.stringify(pos2))
-
-		// update speed
-		speed1 = getDirection(speed1, lines[pos1[1]][pos1[0]])
-		speed2 = getDirection(speed2, lines[pos2[1]][pos2[0]])
+		// left
+		if (x > 0 && !visited[y][x - 1]) {
+			visited[y][x - 1] = true
+			queue.push([x - 1, y])
+		}
+		// right
+		if (x < visited[0].length - 1 && !visited[y][x + 1]) {
+			visited[y][x + 1] = true
+			queue.push([x + 1, y])
+		}
+		// top
+		if (y > 0 && !visited[y - 1][x]) {
+			visited[y - 1][x] = true
+			queue.push([x, y - 1])
+		}
+		// bottom
+		if (y < visited.length - 1 && !visited[y + 1][x]) {
+			visited[y + 1][x] = true
+			queue.push([x, y + 1])
+		}
 	}
-	console.log(`Steps to farthest point: ${visited.size / 2}`)
+}
 
+function solve(lines, x, y) {
+	const visited = Array.from(new Array(lines.length), () => new Array(lines[0].length).fill(false))
+	visited[y][x] = true
+
+	let cnt = 1
+	while (true) {
+		const c = lines[y][x]
+		const up = c === '|' || c === 'L' || c === 'J'
+		const down = c === '|' || c === 'F' || c === '7'
+		const left = c === '-' || c === 'J' || c === '7'
+		const right = c === '-' || c === 'L' || c === 'F'
+
+		if (up && !visited[y - 1][x]) {
+			y -= 1
+			visited[y][x] = true
+			cnt += 1
+			continue
+		}
+
+		if (down && !visited[y + 1][x]) {
+			y += 1
+			visited[y][x] = true
+			cnt += 1
+			continue
+		}
+
+		if (left && !visited[y][x - 1]) {
+			x -= 1
+			visited[y][x] = true
+			cnt += 1
+			continue
+		}
+
+		if (right && !visited[y][x + 1]) {
+			x += 1
+			visited[y][x] = true
+			cnt += 1
+			continue
+		}
+		break
+	}
+
+	console.log(`Path to farthest cell: ${cnt / 2 / 3}`)
+
+	// fill
+	fill(0, 0, visited)
+
+	// TO VISUALIZE THE GRAPH AFTER FILLED
+	// for (const [i, line] of visited.entries()) {
+	// 	for (let j = 0; j < line.length; j += 1) {
+	// 		if (visited[i][j]) {
+	// 			// notFilled += 1
+	// 			visited[i][j] = '#'
+	// 		} else {
+	// 			visited[i][j] = '.'
+	// 		}
+	// 	}
+	// }
+	// console.log(visited.map(line => line.join('')))
+
+	let tmpArr = []
+	let ans = 0
+	// scale down and count the 'real' cell
+	for (let i = 0; i < lines.length; i += 3) {
+		let tmpLine = []
+		for (let j = 0; j < lines[i].length; j += 3) {
+			tmpLine.push(visited[i+1][j+1])
+			if (!visited[i+1][j+1]) ans += 1
+		}
+		tmpArr.push(tmpLine)
+	}
+	// console.log(tmpArr.map(x => x.join('')))
+	console.log(ans)
 }
 
 const inputText = readFileSync('./input.txt', { encoding: 'utf-8' })
 const lines = inputText.split('\n')
 
-// hardcode this because it's just 2 cases...
-solve(lines, [25, 42], [24, 42], [25, 43], [0, -1], [1, 0])
+// hardcoded for my input
+let start = []
+for (const [i, line] of lines.entries()) {
+	if (line.search('S') !== -1) {
+		start.push(line.search('S'), i)
+		lines[i] = lines[i].replace('S', '7')
+	}
+}
+
+const scaledLines = scaleUp(lines)
+solve(scaledLines, start[0] * 3 + 1, start[1] * 3 + 1)
+
